e5f5b1dcf763919a159694e6a0ebf5fd20122af8
eaniko <69327188+eaniko@users.noreply.github.com>
2022-02-09 19:50:52 +0100

    creating data models

---
+++ webshop/src/app/model/order.ts
@@ -0,0 +1,10 @@
+import { Status } from "./status";
+
+export class Order {
+    [key: string]: any;
+    id: number = 0;
+    customerID: number = 0;
+    productID: number = 0;
+    amount: number = 0;
+    status: Status = Status.new|Status.paid|Status.shipped;
+}
