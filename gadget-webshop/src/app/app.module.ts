c997b111c517377a0b2fafde0996c3f4bb8766df
Zolival <zolinak@gmail.com>
2022-02-12 00:01:33 +0100

    routing 01

--- gadget-webshop/src/app/app.module.ts
+++ gadget-webshop/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +12,22 @@ import { BaseListComponent } from './common/base-list/base-list.component';
 import { KeysPipe } from './pipe/keys.pipe';
 import { ProductsComponent } from './page/products/products.component';
 
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  /*
+  {
+    path: 'product',
+    component: ProductsComponent
+  },
+  */
+  {
+    path: '**',
+    component: HomeComponent
+  },
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,7 +42,8 @@ import { ProductsComponent } from './page/products/products.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
