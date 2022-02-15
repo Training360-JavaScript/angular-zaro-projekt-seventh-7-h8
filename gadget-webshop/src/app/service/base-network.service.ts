b8bd93dd69b85354e49ebfee22bd8f3d3134f481
Daniel Kovacs <info@kovix.info>
2022-02-15 21:21:11 +0100

    Add functions to flatten base service response object

--- gadget-webshop/src/app/service/base-network.service.ts
+++ gadget-webshop/src/app/service/base-network.service.ts
@@ -4,6 +4,10 @@ import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { Entity } from '../model/entity';
 
+interface GenericFlattenObj {
+  [key: string]: any;
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -31,6 +35,36 @@ export class BaseNetworkService<GenericEntity extends Entity> {
 
   delete(id: number): Observable<GenericEntity> {
     return this.http.delete<GenericEntity>(`${this.backendURL}${this.endpoint}/${id}`);
+  }
+
+  flattenResponse(entity: GenericEntity, prefix: string = ''): GenericEntity {
+    for (const [key, value] of Object.entries(entity)) {
+      if (typeof value === 'object') {
+        entity = {
+          ...entity,
+          ...this.flattenObject(value, key)
+        }
+        delete entity[key];
+      }
+    }
+    return entity;
+  }
+
+  flattenObject(obj: GenericFlattenObj, prefix: string ): GenericFlattenObj {
+    let ret: GenericFlattenObj = {};
+
+    for (const [key, value] of Object.entries(obj)) {
+      if (typeof value === 'object') {
+        ret = {
+          ...ret,
+          ...this.flattenObject(value, `${prefix}.${key}`)
+        }
+        delete obj[key];
+      } else {
+        ret[`${prefix}.${key}`] = value;
+      }
+    }
+    return ret;
   }
 
 }
