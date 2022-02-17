0490889a5fa8a63b5a979567ff3d26794034e6da
Hajni83 <maro.hajnalka@gmail.com>
2022-02-17 22:17:53 +0100

    delete button service call

--- gadget-webshop/src/app/page/customer/customer-list/customer-list.component.ts
+++ gadget-webshop/src/app/page/customer/customer-list/customer-list.component.ts
@@ -95,6 +95,7 @@ export class CustomerListComponent implements OnInit {
         this.router.navigate([`/${this.routeBase}/edit`, evt.entityID]);
         break;
       case 'DELETE':
+        this.customerService.delete(evt.entityID);
         this.toastr.error(`Got event ${evt.eventID} for entity ${evt.entityID}`, 'Here we should delete this record', {
           positionClass: 'toast-bottom-right'
         });
