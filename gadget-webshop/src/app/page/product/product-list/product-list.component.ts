014b45ccbef02c3fc8f7ffa4cc05850f5f2f2e3a
Daniel Kovacs <info@kovix.info>
2022-02-17 14:47:54 +0100

    Move demo buttons from product to dashboard

--- gadget-webshop/src/app/page/product/product-list/product-list.component.ts
+++ gadget-webshop/src/app/page/product/product-list/product-list.component.ts
@@ -1,10 +1,6 @@
 
 import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
-import { BillService } from 'src/app/service/bill.service';
-import { CategoryService } from 'src/app/service/category.service';
-import { CustomerService } from 'src/app/service/customer.service';
-import { OrderService } from 'src/app/service/order.service';
 import { ProductService } from 'src/app/service/product.service';
 import { Product } from 'src/app/model/product';
 import { ColumnDefinition } from 'src/app/model/column-definition';
@@ -23,7 +19,6 @@ export class ProductListComponent implements OnInit {
 
   products$:Observable<Product[]>= this.ProductServiceTest.getAll();
 
-  public testOutput: string = '';
 
   public columnDefinition: ColumnDefinition[] = [
     new ColumnDefinition({
@@ -66,11 +61,7 @@ export class ProductListComponent implements OnInit {
   ];
 
   constructor(
-    private CategoryServiceTest: CategoryService,
     private ProductServiceTest: ProductService,
-    private CustomerServiceTest: CustomerService,
-    private OrderServiceTest: OrderService,
-    private BillServiceTest: BillService,
     private toastr: ToastrService
   ) {}
 
@@ -83,38 +74,6 @@ export class ProductListComponent implements OnInit {
         positionClass: 'toast-bottom-right'
       });
     })
-  }
-
-  //Ezek tesztek, törölhetőek majd a megfelelő importokkal együtt.
-  getAllCategoryTest(): void {
-    this.CategoryServiceTest.getAll().forEach(response => {this.testOutput = JSON.stringify(response, null, '\t')});
-  }
-  getOneCategoryTest(): void {
-    this.CategoryServiceTest.get(1).forEach(response => {this.testOutput = JSON.stringify(response, null, '\t')});
-  }
-  getOneProductTest(): void {
-    this.ProductServiceTest.get(1).forEach(response => {this.testOutput = JSON.stringify(response, null, '\t')});
-  }
-  getAllProductTest(): void {
-    this.ProductServiceTest.getAll().forEach(response => {this.testOutput = JSON.stringify(response, null, '\t')});
-  }
-  getOneCustomerTest(): void {
-    this.CustomerServiceTest.get(1).forEach(response => {this.testOutput = JSON.stringify(response, null, '\t')});
-  }
-  getAllCustomerTest(): void {
-    this.CustomerServiceTest.getAll().forEach(response => {this.testOutput = JSON.stringify(response, null, '\t')});
-  }
-  getOneOrderTest(): void {
-    this.OrderServiceTest.get(1).forEach(response => {this.testOutput = JSON.stringify(response, null, '\t')});
-  }
-  getAllOrderTest(): void {
-    this.OrderServiceTest.getAll().forEach(response => {this.testOutput = JSON.stringify(response, null, '\t')});
-  }
-  getOneBillTest(): void {
-    this.BillServiceTest.get(1).forEach(response => {this.testOutput = JSON.stringify(response, null, '\t')});
-  }
-  getAllBillTest(): void {
-    this.BillServiceTest.getAll().forEach(response => {this.testOutput = JSON.stringify(response, null, '\t')});
   }
 
 }
