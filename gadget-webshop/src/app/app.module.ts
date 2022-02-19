ea4cc5b0eb8b3b3e3aff6d06b1975e4c246ad827
Daniel Kovacs <info@kovix.info>
2022-02-19 08:08:33 +0100

    clean up app.module

--- gadget-webshop/src/app/app.module.ts
+++ gadget-webshop/src/app/app.module.ts
@@ -1,19 +1,19 @@
-import { FilterPipe } from './pipe/filter.pipe';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser'
 import { HttpClientModule } from '@angular/common/http';
-
-import { RouterModule, Routes } from '@angular/router'
-import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ToastrModule } from 'ngx-toastr';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 import { SidebarComponent } from './common/sidebar/sidebar.component';
-import { HomeComponent } from './page/home/home.component';
 import { BaseListComponent } from './common/base-list/base-list.component';
+import { EditProductFormComponent } from './common/edit-product-form/edit-product-form.component';
+import { ProductSelectorComponent } from './common/order/product-selector/product-selector.component';
+import { HomeComponent } from './page/home/home.component';
 import { ProductListComponent } from './page/product/product-list/product-list.component';
 import { CustomerListComponent } from './page/customer/customer-list/customer-list.component';
 import { OrderListComponent } from './page/order/order-list/order-list.component';
@@ -22,11 +22,10 @@ import { EditBillComponent } from './page/bill/edit-bill/edit-bill.component';
 import { EditCustomerComponent } from './page/customer/edit-customer/edit-customer.component';
 import { EditOrderComponent } from './page/order/edit-order/edit-order.component';
 import { EditProductComponent } from './page/product/edit-product/edit-product.component';
-import { FormsModule } from '@angular/forms';
-import { SortPipe } from './pipe/sort.pipe';
 import { ViewOrderComponent } from './page/order/view-order/view-order.component';
-import { EditProductFormComponent } from './common/edit-product-form/edit-product-form.component';
 
+import { FilterPipe } from './pipe/filter.pipe';
+import { SortPipe } from './pipe/sort.pipe';
 
 @NgModule({
   declarations: [
@@ -46,7 +45,8 @@ import { EditProductFormComponent } from './common/edit-product-form/edit-produc
     FilterPipe,
     SortPipe,
     ViewOrderComponent,
-    EditProductFormComponent
+    EditProductFormComponent,
+    ProductSelectorComponent
   ],
   imports: [
     BrowserModule,
